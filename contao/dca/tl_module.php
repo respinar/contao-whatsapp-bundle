19783f042d5386b5e528cdace3f5606b7c0efc4d
Hamid Peywasti <hamid@respinar.com>
2025-03-11 19:47:26 +0330

    remove translation fields

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -10,21 +10,18 @@ $GLOBALS["TL_DCA"]["tl_module"]["palettes"]["whatsapp"] = '
 ';
 
 $GLOBALS["TL_DCA"]["tl_module"]["fields"]["whatsappTitle"] = [
-    "label" => &$GLOBALS["TL_LANG"]["tl_module"]["whatsappTitle"],
     "inputType" => "text",
     "eval" => ["mandatory" => true, "maxlength" => 255, "tl_class" => "w50"],
     "sql" => "varchar(255) NOT NULL default ''",
 ];
 
 $GLOBALS["TL_DCA"]["tl_module"]["fields"]["whatsappNumber"] = [
-    "label" => &$GLOBALS["TL_LANG"]["tl_module"]["whatsappNumber"],
     "inputType" => "text",
     "eval" => ["mandatory" => true, "maxlength" => 20, "tl_class" => "w50 clr"],
     "sql" => "varchar(20) NOT NULL default ''",
 ];
 
 $GLOBALS["TL_DCA"]["tl_module"]["fields"]["whatsappMessage"] = [
-    "label" => &$GLOBALS["TL_LANG"]["tl_module"]["whatsappMessage"],
     "inputType" => "text",
     "eval" => ["mandatory" => true, "maxlength" => 255, "tl_class" => "w50"],
     "sql" => "varchar(255) NOT NULL default ''",
