0f0a69e3b1d68c013bab98ea46790c4c7330c26e
Hamid Peywasti <hamid@respinar.com>
2025-03-11 19:47:32 +0330

    remove translation fields

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -22,7 +22,6 @@ PaletteManipulator::create()
 
 // Define fields
 $GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappDisabled"] = [
-    "label" => &$GLOBALS["TL_LANG"]["tl_page"]["whatsappDisabled"],
     "exclude" => true,
     "inputType" => "checkbox",
     "eval" => ["tl_class" => "w50 m12"],
@@ -30,7 +29,6 @@ $GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappDisabled"] = [
 ];
 
 $GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappTitle"] = [
-    "label" => &$GLOBALS["TL_LANG"]["tl_page"]["whatsappTitle"],
     "exclude" => true,
     "inputType" => "text",
     "eval" => ["maxlength" => 255, "tl_class" => "w50"],
@@ -38,7 +36,6 @@ $GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappTitle"] = [
 ];
 
 $GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappNumber"] = [
-    "label" => &$GLOBALS["TL_LANG"]["tl_page"]["whatsappNumber"],
     "exclude" => true,
     "inputType" => "text",
     "eval" => ["rgxp" => "phone", "maxlength" => 20, "tl_class" => "w50"],
@@ -46,7 +43,6 @@ $GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappNumber"] = [
 ];
 
 $GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappMessage"] = [
-    "label" => &$GLOBALS["TL_LANG"]["tl_page"]["whatsappMessage"],
     "exclude" => true,
     "inputType" => "text",
     "eval" => ["maxlength" => 255, "tl_class" => "w50"],
