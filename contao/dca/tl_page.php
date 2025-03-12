18f52a067c60c5e91aaecece9b7da171d187c2c4
Hamid Peywasti <hamid@respinar.com>
2025-03-12 17:55:27 +0330

    convert sql to Doctrine Schema Representation

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -25,26 +25,26 @@ $GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappDisabled"] = [
     "exclude" => true,
     "inputType" => "checkbox",
     "eval" => ["tl_class" => "w50 m12"],
-    "sql" => "char(1) NOT NULL default ''",
+    "sql" => ['type' => 'boolean', 'default' => false],
 ];
 
 $GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappTitle"] = [
     "exclude" => true,
     "inputType" => "text",
     "eval" => ["maxlength" => 255, "tl_class" => "w50"],
-    "sql" => "varchar(255) NOT NULL default ''",
+    "sql" => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
 
 $GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappNumber"] = [
     "exclude" => true,
     "inputType" => "text",
     "eval" => ["rgxp" => "phone", "maxlength" => 20, "tl_class" => "w50"],
-    "sql" => "varchar(20) NOT NULL default ''",
+    "sql" => ['type' => 'string', 'length' => 20, 'default' => ''],
 ];
 
 $GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappMessage"] = [
     "exclude" => true,
     "inputType" => "text",
     "eval" => ["maxlength" => 255, "tl_class" => "w50"],
-    "sql" => "varchar(255) NOT NULL default ''",
+    "sql" => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
