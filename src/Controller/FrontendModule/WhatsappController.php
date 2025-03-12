b08a780357715339f60f8e359bdd4763359c1135
Hamid Peywasti <hamid@respinar.com>
2025-03-12 17:56:05 +0330

    consider also config from root page

--- src/Controller/FrontendModule/WhatsappController.php
+++ src/Controller/FrontendModule/WhatsappController.php
@@ -8,6 +8,7 @@ use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
 use Contao\CoreBundle\Twig\FragmentTemplate;
 use Contao\ModuleModel;
+use Contao\PageModel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Contao\StringUtil;
@@ -20,16 +21,23 @@ class WhatsappController extends AbstractFrontendModuleController
         // Check if the current page has WhatsApp fields set
         $page = $this->getPageModel();
 
-        if ($page->whatsappDisabled) {
+        // Get the root page
+        $rootPage = PageModel::findById($page->rootId);
+        if ($rootPage === null) {
+            // Log an error or handle gracefully; for now, assume CTA is disabled
             return new Response();
         }
+       
+         if ($page->whatsappDisabled ?? $rootPage->whatsappDisabled ?? false) {
+            return new Response();
+        }       
 
         // Assign data to the template
-        $template->whatsappTitle = $page->whatsappTitle ?: $model->whatsappTitle;
-        $template->whatsappNumber = $page->whatsappNumber ?: $model->whatsappNumber;
-        $template->whatsappMessage = $page->whatsappMessage ?: $model->whatsappMessage;
-        $template->cssClass = StringUtil::deserialize($model->cssID)[1] ?? '';
-        $template->searchable = False;
+        $template->set('whatsappTitle', $page->whatsappTitle ?: $rootPage->whatsappTitle ?: $model->whatsappTitle);
+        $template->set('whatsappNumber', $page->whatsappNumber ?: $rootPage->whatsappNumber ?: $model->whatsappNumber);
+        $template->set('whatsappMessage', $page->whatsappMessage ?: $rootPage->whatsappMessage ?: $model->whatsappMessage);
+        $template->set('cssClass', StringUtil::deserialize($model->cssID)[1] ?? '');
+        $template->set('searchable', False);
 
         // Add JavaScript file to the page
         $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/respinarcontaowhatsapp/js/whatsapp.js|static';
